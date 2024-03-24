781de727f81525ce1258cf753537815b3ac52fe6
LordAkirast <darkwarish@gmail.com>
2024-03-24 15:24:13 -0300

    feat: rota de delete implementada

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import createTenant from './controllers/create.controller';
 import readTenants from './controllers/read.controller';
 import updateTenants from './controllers/update.controller';
+import deleteTenant from './controllers/delete.controller';
 
 const app = express();
 app
@@ -11,7 +12,8 @@ app
   .get('/health', (_req, res) => res.send('OK!'))
   .post('/create', createTenant)
   .get('/read', readTenants)
-  .patch('/update', updateTenants);
+  .patch('/update', updateTenants)
+  .delete('/delete', deleteTenant)
 
 app.listen(4000, () => console.log('Servidor rodando na porta 4000'))
 
---
+++ src/controllers/delete.controller.ts
@@ -0,0 +1,42 @@
+import { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+async function deleteTenant(req: Request, res: Response) {
+
+    const tenantId = req.body.id;
+
+    console.log('1')
+
+    try {
+        console.log('2')
+
+        console.log(tenantId)
+
+        const idVerify = await prisma.tenants.findFirst({
+            where: { id: tenantId }
+
+        })
+        console.log('3')
+
+        if (!idVerify) {
+            console.log('4')
+            return res.status(404).send("User ID não encontrado.")
+        } else {
+            console.log('5')
+
+            const deletion = await prisma.tenants.delete({
+                where: { id: tenantId }
+            })
+            return res.status(204).send(`Usuário de ID ${tenantId} deletado com sucesso.`)
+        }
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+
+
+
+}
+
+export default deleteTenant
